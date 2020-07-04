02cad10e8c7f0af3fc19c73e88106eed8315db86
Pablo Garcia <pgarcia@onfocusdev.com>
2020-07-04 09:46:09 -0300

    Change text Cargando for Loading

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import LoadingGIF from '../../assets/loading.gif'
 import './Loading.scss'
 
-const Loading = () => (
+import LoadingGIF from '../../assets/loading.gif'
+
+const Loading = (): JSX.Element => (
     <div className="loading-container">
         <img src={LoadingGIF} alt="LOADING" className="loading" />
-        <span className="loading-label">Cargando...</span>
+        <span className="loading-label">Loading...</span>
     </div>
 )
 
--- src/components/movie-card/MovieCard.tsx
+++ src/components/movie-card/MovieCard.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react'
+import React, { useRef, useEffect } from 'react'
 import { TimelineLite } from 'gsap'
 
 import './MovieCard.scss'
