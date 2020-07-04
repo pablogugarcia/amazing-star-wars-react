003b57c5a00223c24fd8cf3744c8509df8c16752
Pablo Garcia <pgarcia@onfocusdev.com>
2020-07-04 09:55:31 -0300

    Test eslint prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Route } from 'react-router-dom'
 import HomeScreen from './screens/home/HomeScreen'
 import NavBar from './components/navbar/Navbar'
@@ -7,48 +7,40 @@ import FilmScreen from './screens/films/FilmScreen'
 import PeopleScreen from './screens/people/PeopleScreen'
 import MainLayout from './layouts/MainLayout'
 
-const App = (): JSX.Element => {
-    useEffect(() => {
-        fetch('https://swapi.dev/api/films/1/')
-            .then((r) => r.json())
-            .then((r) => console.log(r))
-    }, [])
-
-    return (
-        <div>
-            <NavBar items={['Films', 'People']} />
-            <div style={{ display: 'flex', justifyContent: 'space-around ' }}>
-                <div
-                    style={{
-                        border: '1px solid #e2e2e2',
-                        margin: '5rem 5rem 1rem 5rem',
-                        width: '10%',
-                    }}
-                />
-                <div
-                    style={{
-                        border: '1px solid #e2e2e2',
-                        margin: '5rem 5rem 1rem 5rem',
-                        width: '10%',
-                    }}
-                />
-            </div>
-            <Particles />
-            <Route path="/Films">
-                <MainLayout title="FILMS">
-                    <FilmScreen />
-                </MainLayout>
-            </Route>
-            <Route path="/People">
-                <MainLayout title="PEOPLE">
-                    <PeopleScreen />
-                </MainLayout>
-            </Route>
-            <Route path="/" exact>
-                <HomeScreen />
-            </Route>
-        </div>
-    )
-}
+const App = (): JSX.Element => (
+  <div>
+    <NavBar items={['Films', 'People']} />
+    <div style={{ display: 'flex', justifyContent: 'space-around ' }}>
+      <div
+        style={{
+          border: '1px solid #e2e2e2',
+          margin: '5rem 5rem 1rem 5rem',
+          width: '10%',
+        }}
+      />
+      <div
+        style={{
+          border: '1px solid #e2e2e2',
+          margin: '5rem 5rem 1rem 5rem',
+          width: '10%',
+        }}
+      />
+    </div>
+    <Particles />
+    <Route path="/Films">
+      <MainLayout title="FILMS">
+        <FilmScreen />
+      </MainLayout>
+    </Route>
+    <Route path="/People">
+      <MainLayout title="PEOPLE">
+        <PeopleScreen />
+      </MainLayout>
+    </Route>
+    <Route path="/" exact>
+      <HomeScreen />
+    </Route>
+  </div>
+)
 
 export default App
--- src/screens/films/FilmScreen.tsx
+++ src/screens/films/FilmScreen.tsx
@@ -5,100 +5,71 @@ import { Film } from '../../models/api/Api'
 import MovieCard from '../../components/movie-card/MovieCard'
 import Loading from '../../components/loading/Loading'
 
-/**
- * 1 - Nombre
-2 - Número de episodio
-3 - Director
-4 - Personajes (Enlace que me permita ir a la página de personajes del film)
-5 - Extra bonus (Al hacer click/over sobre el film mostrar los textos con los que inicia el film)
-6 - Super Extra bonus (Animar el texto https://giphy.com/media/2UCCiILkXaLbDbrjll/giphy.gif
-
-    title string -- The title of this film
-episode_id integer -- The episode number of this film.
-opening_crawl string -- The opening paragraphs at the beginning of this film.
-director string -- The name of the director of this film.
-producer string -- The name(s) of the producer(s) of this film. Comma separated.
-release_date date -- The ISO 8601 date format of film release at original creator country.
-species array -- An array of species resource URLs that are in this film.
-starships array -- An array of starship resource URLs that are in this film.
-vehicles array -- An array of vehicle resource URLs that are in this film.
-characters array -- An array of people resource URLs that are in this film.
-planets array -- An array of planet resource URLs that are in this film.
-url string -- the hypermedia URL of this resource.
-created string -- the ISO 8601 date format of the time that this resource was created.
-edited string -- the ISO 8601 date format of the time that this resource was edited.
- */
-
 const FilmScreen = (): JSX.Element => {
-    const [films, setFilms] = useState<(Film & { isSelected?: boolean })[]>([])
-    useEffect(() => {
-        const id = setTimeout(() => {
-            fetch('https://swapi.dev/api/films')
-                .then((r) => r.json())
-                .then((data) => {
-                    setFilms(data.results)
-                })
-                .catch((e) => alert('error'))
-        }, 10000)
+  const [films, setFilms] = useState<(Film & { isSelected?: boolean })[]>([])
+  useEffect(() => {
+    const id = setTimeout(() => {
+      fetch('https://swapi.dev/api/films')
+        .then((r) => r.json())
+        .then((data) => {
+          setFilms(data.results)
+        })
+        .catch(() => undefined) // for now
+    }, 10000)
 
-        return () => clearTimeout(id)
-    }, [])
+    return () => clearTimeout(id)
+  }, [])
 
-    const handleCardClick = (title: string) => () => {
-        console.log('click-->', films)
-        setFilms((prevState) =>
-            prevState.map((film) => {
-                if (film.title === title) {
-                    const newFilm = { ...film }
-                    newFilm.isSelected = !film.isSelected
-                    return newFilm
-                }
-                return film
-            })
-        )
-    }
-
-    if (films.length === 0) {
-        return <Loading />
-    }
-
-    return (
-        <div className="film-screen-container">
-            {films.map((film) => (
-                <Card
-                    key={film.title}
-                    style={{
-                        gridColumnStart: film.isSelected ? 'span 3' : 'unset',
-                    }}
-                    onClick={handleCardClick(film.title)}
-                >
-                    <CardContent className="film-card-content">
-                        <div>
-                            <p>
-                                <span className="film-label">Name: </span>
-                                {film.title}
-                            </p>
-                            <p>
-                                <span className="film-label">
-                                    Episode number :{' '}
-                                </span>
-                                {film.episode_id}
-                            </p>
-                            <p>
-                                <span className="film-label">Director: </span>
-                                {film.director}
-                            </p>
-                            <p>link</p>
-                        </div>
-
-                        {film.isSelected && (
-                            <MovieCard text={film.opening_crawl} />
-                        )}
-                    </CardContent>
-                </Card>
-            ))}
-        </div>
+  const handleCardClick = (title: string) => () => {
+    setFilms((prevState) =>
+      prevState.map((film) => {
+        if (film.title === title) {
+          const newFilm = { ...film }
+          newFilm.isSelected = !film.isSelected
+          return newFilm
+        }
+        return film
+      })
     )
+  }
+
+  if (films.length === 0) {
+    return <Loading />
+  }
+
+  return (
+    <div className="film-screen-container">
+      {films.map((film) => (
+        <Card
+          key={film.title}
+          style={{
+            gridColumnStart: film.isSelected ? 'span 3' : 'unset',
+          }}
+          onClick={handleCardClick(film.title)}
+        >
+          <CardContent className="film-card-content">
+            <div>
+              <p>
+                <span className="film-label">Name: </span>
+                {film.title}
+              </p>
+              <p>
+                <span className="film-label">Episode number : </span>
+                {film.episode_id}
+              </p>
+              <p>
+                <span className="film-label">Director: </span>
+                {film.director}
+              </p>
+              <p>link</p>
+            </div>
+
+            {film.isSelected && <MovieCard text={film.opening_crawl} />}
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  )
 }
 
 export default FilmScreen
