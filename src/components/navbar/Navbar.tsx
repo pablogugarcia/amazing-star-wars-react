2625768eabce8d34997925c2ddf60d2072c0d080
Pablo Garcia <pgarcia@onfocusdev.com>
2020-07-05 13:56:27 -0300

    Remove unused fonts , add icon and title page

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+
 import styles from './Navbar.module.css'
 import { NavbarProps } from './Navbar.model'
 import HomeIMG from '../../assets/home.svg'
