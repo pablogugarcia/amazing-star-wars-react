1d79719abe8a3fbb91d279bf435b9eae8806dcf9
Pablo Garcia <pgarcia@onfocusdev.com>
2020-07-05 00:09:14 -0300

    Updates

---
+++ precache-manifest.01ecff9aad2be1fc09d6a0edc35bc7fd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04584cf2f1953a07ba972755fb7fbb3f",
+    "url": "/pablogugarcia/amazing-star-wars-react/index.html"
+  },
+  {
+    "revision": "4990352b5348ad8a8990",
+    "url": "/pablogugarcia/amazing-star-wars-react/static/css/2.cef8191b.chunk.css"
+  },
+  {
+    "revision": "058e46b8474e3afdb09c",
+    "url": "/pablogugarcia/amazing-star-wars-react/static/css/main.24e6eb37.chunk.css"
+  },
+  {
+    "revision": "4990352b5348ad8a8990",
+    "url": "/pablogugarcia/amazing-star-wars-react/static/js/2.9658b51b.chunk.js"
+  },
+  {
+    "revision": "d0bb7e5696919618c326a7d7a41bf652",
+    "url": "/pablogugarcia/amazing-star-wars-react/static/js/2.9658b51b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "058e46b8474e3afdb09c",
+    "url": "/pablogugarcia/amazing-star-wars-react/static/js/main.53fe22bc.chunk.js"
+  },
+  {
+    "revision": "c505bbc886cc2ebba4e0",
+    "url": "/pablogugarcia/amazing-star-wars-react/static/js/runtime-main.7c30fd0c.js"
+  },
+  {
+    "revision": "87954d9a3f9636afff329ac93c19c4b4",
+    "url": "/pablogugarcia/amazing-star-wars-react/static/media/home.87954d9a.svg"
+  },
+  {
+    "revision": "c2ec3eed2ebc126e912aedbcec12a7da",
+    "url": "/pablogugarcia/amazing-star-wars-react/static/media/lightsaber.c2ec3eed.svg"
+  },
+  {
+    "revision": "cd795592c8a203a06cf68b46f239721b",
+    "url": "/pablogugarcia/amazing-star-wars-react/static/media/loading.cd795592.gif"
+  }
+]);
